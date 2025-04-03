ee2d35710ebc2d563ce45a90b9121a91a9146104
Mogom <kevin_spalacios@soy.sena.edu.co>
2025-04-03 09:15:43 -0500

    Hola si

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route, Link} from "react-router"
 import './App.css';
+import NavBar from "./Components/NavBar";
+// import { Home, Formato, Ayuda, Ver, Archivo, Edicion } from "./Pages";
+import { Home } from "./Pages/Home";
+import { Ayuda } from "./Pages/Ayuda";
+import { Edicion } from "./Pages/Edicion";
+import { Formato } from "./Pages/Formato";
+import { Ver } from "./Pages/Ver";
+import { Archivo } from "./Pages/Archivo";
+import { useState } from "react";
+import { Protegidas } from "./Components/Protected";
+import { ProtegidasRol } from "./Components/ProtectedRol";
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
+  const login=(data)=>{
+    setUser(data)
+  }
+
+  const users=[
+    {
+      id :1,
+      nombre :"Kevin",
+      rol:"Admin"
+    },
+    
+    {
+    id :2,
+    nombre :"Critian",
+    rol:"User"
+  }
+  ]
+
+  const logout =()=>{setUser(null)}
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar/>
+
+        {user? (
+          <button onClick={logout}>Logout</button>
+        ):(
+          <button onClick={() => login(users[0])}>Login</button>
+        )}
+        {console.log(user)}
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route element={< Protegidas userIn={user}/>} >
+            <Route path="archivo" element={<Archivo />} />
+            <Route path="edicion" element={<Edicion />} />
+          </Route>
+          <Route element={<ProtegidasRol userIn={user} />} >
+            <Route path="formato" element={<Formato />} />
+            <Route path="ver" element={<Ver />} />
+            <Route path="ayuda" element={<Ayuda />} />
+          </Route>
+          
+          {/* <Route path="*" element={<NoPage />} /> */}
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
+
+
 
 export default App;
